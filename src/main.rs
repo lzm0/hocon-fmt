68b1fbf832dc8d98f3d58fa41252319a065e4150
Zimo Li <7163127+lzm0@users.noreply.github.com>
2025-04-16 10:48:12 -0400

    Format pair

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,22 @@ fn main() {
     let parse_result = HoconParser::parse(Rule::hocon, &content);
     match parse_result {
         Ok(pairs) => {
-            println!("{:?}", pairs);
+            pairs.for_each(|pair| format_pair(pair));
         }
         Err(e) => {
             println!("{}", e);
             std::process::exit(1);
+        }
+    }
+}
+
+fn format_pair(pair: pest::iterators::Pair<Rule>) {
+    for inner_pair in pair.into_inner() {
+        match inner_pair.as_rule() {
+            _ => {
+                println!("{:?}", inner_pair.as_str());
+                format_pair(inner_pair);
+            }
         }
     }
 }
